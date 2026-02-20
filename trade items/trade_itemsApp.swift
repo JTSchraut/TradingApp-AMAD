8859085a15fdbfaa0608839e3294ae1fd01f9285
shanejcoffey <shanejcoffey27@gmail.com>
2026-02-20 08:07:42 -0600

    Initial Commit

---
+++ trade items/trade_itemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  trade_itemsApp.swift
+//  trade items
+//
+//  Created by SHANE COFFEY on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct trade_itemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
