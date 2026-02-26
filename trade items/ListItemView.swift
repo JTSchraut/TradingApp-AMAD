a20e490ac10d2bd858bb9d011c52e5c172249cb2
James Schraut <JT@Schraut.org>
2026-02-26 08:10:27 -0600

    Post items

---
+++ trade items/ListItemView.swift
@@ -0,0 +1,18 @@
+//
+//  PostItemView.swift
+//  trade items
+//
+//  Created by JAMES SCHRAUT on 2/26/26.
+//
+
+import SwiftUI
+
+struct PostItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostItemView()
+}
